bdb31b24bab5ed0449828b834fca6b85ff30fe55
Michael Hoff <hoff@igi.tugraz.at>
2017-03-24 16:49:59 +0100

    Abort if doxygen failed.

--- update-docs.sh
+++ update-docs.sh
@@ -1,5 +1,9 @@
 cd .docs
 doxygen
+if [ ! $? = 0 ]; then
+    echo "Running doxygen failed. Aborting."
+    exit 1
+fi
 git add *
 git commit -m "Update docs to revision `git rev-parse HEAD`."
 echo "Entering sub-shell. You can now check the docs for integrity. Press CTRL+D to continue."
