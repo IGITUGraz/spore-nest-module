f1f3ce3d4e796b8eb79598a6d243b951d59265c5
Kappel David <kappel@node5.pCluster>
2017-03-28 14:09:25 +0200

    fixed formatting and warning

--- test_tracing_node.cpp
+++ test_tracing_node.cpp
@@ -14,22 +14,23 @@ namespace spore
  * Constructor.
  */
 TestTracingNode::TestTracingNode()
-:SporeTestBase("test_tracing_node")
-{}
+: SporeTestBase("test_tracing_node")
+{
+}
 
 /**
  * Execute once at startup.
  */
 void TestTracingNode::check(nest::delay time_step, TracingNode *node)
 {
-    test_assert(node->get_num_traces()==5, "number of traces");
-    
-    for (size_t tid=0; tid<node->get_num_traces(); tid++)
+    test_assert(node->get_num_traces() == 5, "number of traces");
+
+    for (size_t tid = 0; tid < node->get_num_traces(); tid++)
     {
-        for (size_t t=0; t<time_step; t++)
+        for (nest::delay t = 0; t < time_step; t++)
         {
             //std::cout << (*node->get_trace(t, tid)) << " <-> " << t+tid << std::endl;
-            test_assert( (*node->get_trace(t, tid))==t+tid, "trace content");
+            test_assert((*node->get_trace(t, tid)) == t + tid, "trace content");
         }
     }
 }
