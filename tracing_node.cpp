2daf7649de5a21a10a33ac2b21988fe365bc4bdb
Michael Hoff <hoff@igi.tugraz.at>
2017-03-29 18:06:28 +0200

    Add get trace functionality to tracing node.

--- tracing_node.cpp
+++ tracing_node.cpp
@@ -35,14 +35,16 @@ namespace spore
  * Constructor.
  */
 TracingNode::TracingNode()
-:nest::Node()
-{}
+: nest::Node()
+{
+}
 
 /**
  * Destructor.
  */
 TracingNode::~TracingNode()
-{}
+{
+}
 
 /**
  * Constructor.
@@ -51,8 +53,8 @@ void TracingNode::init_traces(size_t num_traces)
 {
     traces_.resize(num_traces);
     const size_t trace_length = ConnectionUpdateManager::instance()->get_max_latency();
-    
-    for (size_t i=0; i<num_traces; i++)
+
+    for (size_t i = 0; i < num_traces; i++)
     {
         traces_[i].resize(trace_length, 0.0);
     }
@@ -70,6 +72,24 @@ void TracingNode::set_status(const DictionaryDatum& d)
  */
 void TracingNode::get_status(DictionaryDatum& d) const
 {
+    nest::Time time = ConnectionUpdateManager::instance()->get_horizon();
+    int id = 0;
+    def<int>(d, "trace_id", id);
+    if (id < 0)
+    {
+        std::string msg = String::compose("Trace id '%1' illegal", id);
+        throw BadParameterValue(msg);
+    }
+    TracingNode::const_iterator it = get_trace(time, id);
+    std::vector<double> trace;
+    const size_t trace_length = ConnectionUpdateManager::instance()->get_max_latency();
+    trace.resize(trace_length);
+    for (size_t i = 0; i < trace_length; i++)
+    {
+        trace[i] = *it;
+        ++it; // back to the future
+    }
+    (*d)["trace"] = trace;
 }
 
 }
