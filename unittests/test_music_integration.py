34f6586abdc77b1b8105a17d790959bab486ca46
Michael Hoff <hoff@igi.tugraz.at>
2017-04-03 16:09:24 +0200

    Refactor test.

--- unittests/test_music_integration.py
+++ unittests/test_music_integration.py
@@ -5,11 +5,10 @@ import subprocess
 import os
 
 
-class PatternMatchingTest(unittest.TestCase):
-    def test(self):
+class MusicIntegrationTest(unittest.TestCase):
+    def test_pattern_matching(self):
         showcase_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', 'pattern_matching_showcase')
-        print(showcase_path)
-        subprocess.check_call(["mpirun", "-np", "3", "music", "run.music", "TEST_MODE"], cwd=showcase_path)
+        subprocess.check_call(["sh", "experiment.test.sh"], cwd=showcase_path)
 
 if __name__ == '__main__':
     unittest.main()
