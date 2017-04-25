79494b223453ea0ee92b78d1749240f0bba39e07
Michael Hoff <hoff@igi.tugraz.at>
2017-04-25 14:06:24 +0200

    Adapt ignored style-rules.

--- utils/run-style-check.sh
+++ utils/run-style-check.sh
@@ -1,6 +1,6 @@
 mkdir style-reports
 now=$(date +"%m-%d-%Y-%T")
-flake8 --ignore=E501,W293,W291,E265,E231,E127,E262,E241 --max-line-length=120 --exclude style-reports/ --tee --output-file style-reports/report-${now}.txt
+flake8 --ignore=E241,F403,F405 --max-line-length=120 --exclude style-reports/ --tee --output-file style-reports/report-${now}.txt
 # echo $?
 # E501 line too long (89 > 79 characters)
 # W293 blank line contains whitespace
@@ -10,4 +10,6 @@ flake8 --ignore=E501,W293,W291,E265,E231,E127,E262,E241 --max-line-length=120 --
 # E127 continuation line over-indented for visual indent
 # E262 inline comment should start with '# '
 # E241 multiple spaces after ','
+# F403 star-imports
+# F405 variable undefined or from star-import
 exit $?
